ec021764f45998d40bc314c6c444f966fdfa7862
erozabesu <treasure_cats_drift@yahoo.co.jp>
2015-07-14 03:57:20 +0900

    v1.3.0 : Vaultとの連携処理を追加

--- src/com/github/erozabesu/yplkart/YPLKart.java
+++ src/com/github/erozabesu/yplkart/YPLKart.java
@@ -8,6 +8,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.github.erozabesu.yplkart.Cmd.CMD;
+import com.github.erozabesu.yplkart.Connection.VaultConnection;
 import com.github.erozabesu.yplkart.Data.DisplayKartData;
 import com.github.erozabesu.yplkart.Data.Message;
 import com.github.erozabesu.yplkart.Data.RaceData;
@@ -20,7 +21,6 @@ import com.github.erozabesu.yplkart.Utils.ReflectionUtil;
 import com.github.erozabesu.yplkart.Utils.Util;
 
 public class YPLKart extends JavaPlugin{
-	private static YPLKart pl;
 	public static String plname;
 
 	public static FileConfiguration cConfig;
@@ -28,9 +28,10 @@ public class YPLKart extends JavaPlugin{
 	public static FileConfiguration Config;
 	public static File ConfigFile;
 
+	private VaultConnection vaultConnection;
+
 	@Override
 	public void onEnable(){
-		pl = this;
 		plname = this.getDescription().getName();
 		CMD CMDExecutor = new CMD();
 		getCommand("ka").setExecutor(CMDExecutor);
@@ -52,6 +53,9 @@ public class YPLKart extends JavaPlugin{
 			DisplayKartData.respawnKart(w);
 		}
 
+		if (getServer().getPluginManager().isPluginEnabled("Vault")) {
+			this.vaultConnection = VaultConnection.setupConnection(getServer().getPluginManager().getPlugin("Vault"));
+		}
 
 		Message.sendAbsolute(null, "[" + plname + "] v." + YPLKart.getInstance().getDescription().getVersion() + " Plugin has been Enabled");
 	}
@@ -63,7 +67,11 @@ public class YPLKart extends JavaPlugin{
 	}
 
 	public static YPLKart getInstance(){
-		return pl;
+		return (YPLKart) Bukkit.getPluginManager().getPlugin("YPLKart");
+	}
+
+	public VaultConnection getVaultConnection(){
+		return this.vaultConnection;
 	}
 
 	public static File getPluginFile(){
