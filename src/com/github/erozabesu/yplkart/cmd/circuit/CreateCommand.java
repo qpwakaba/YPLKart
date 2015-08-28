63df7b94458cb90f4e23a1c5ce1d05c723ccbcd1
erozabesu <treasure_cats_drift@yahoo.co.jp>
2015-08-29 08:29:38 +0900

    v2.0.6 : circuit createコマンドが引数3個で実行できない不具合を修正

--- src/com/github/erozabesu/yplkart/cmd/circuit/CreateCommand.java
+++ src/com/github/erozabesu/yplkart/cmd/circuit/CreateCommand.java
@@ -19,12 +19,23 @@ public class CreateCommand extends Command {
     // 0:circuit 1:create 2:circuitname 3:worldname 4:x 5:y 6:z 7:pitch 8:yaw
     @Override
     public boolean execute(CommandSender sender, String label, String[] args) {
-        if (!Permission.hasPermission(sender, Permission.OP_CMD_CIRCUIT, false))
+        if (!Permission.hasPermission(sender, Permission.OP_CMD_CIRCUIT, false)) {
             return true;
-        // 引数が9個指定されたときか、プレイヤーが3つ指定したとき
-        if (args.length == 9 || (args.length == 3 && Util.isPlayer(sender)))
-            // TODO: ここでコマンドの説明を表示させられる。
-            return false;
+        }
+
+        if (Util.isPlayer(sender)) {
+            // プレイヤーが引数3個、9個以外のコマンドを実行した場合return falseする
+            if (args.length != 3 && args.length != 9) {
+                // TODO: ここでコマンドの説明を表示させられる。
+                return false;
+            }
+        } else {
+            // プレイヤー以外が引数9個以外のコマンドを実行した場合return falseする
+            if (args.length != 9) {
+                // TODO: ここでコマンドの説明を表示させられる。
+                return false;
+            }
+        }
 
         String circuitName = args[2];
         Location location;
